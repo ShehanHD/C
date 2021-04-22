4dfa2195b291c22b5ba6193ce6cec9693ea90593
ShehanHD <51677228+ShehanHD@users.noreply.github.com>
2021-04-22 23:38:59 +0200

    .

--- charu/main.c
+++ charu/main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define MAX_LIMIT 20
 
 int main()
 {
+
     int a=54325;
     char buffer[20];
     itoa(a,buffer,2);
@@ -14,5 +16,10 @@ int main()
 
     itoa(a,buffer,16);
     printf("Hexadecimal value = %s\n", buffer);
+
+    char str[MAX_LIMIT];
+   fgets(str, MAX_LIMIT, stdin);
+   printf("%s", str);
+
     return 0;
 }
